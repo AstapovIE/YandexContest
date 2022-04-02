81c5b02ff0dbb1a291cbc46cd368cd4c947e0208
Ilya Astapov <iastapov114@gmail.com>
2022-04-02 22:01:50 +0300

    Create 1_6ReverseMass.cpp

---
+++ 2SemAlgos/1_6ReverseMass.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+//Дан массив целых чисел A[0..n). Не используя других массивов переставить элементы массива A в обратном порядке за O(n). n ≤ 10000.
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    int temp = 0;
+    for (int i=0; i<n ;i++){
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    int j = n-1;
+    int buff;
+    for (int i = 0; i <= (n/2)-1; i++) {
+        buff = arr[i];
+        arr[i] = arr[j];
+        arr[j] = buff;
+        j--;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
+
+}
+
