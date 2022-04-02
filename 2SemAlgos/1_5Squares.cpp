b04a30c7a6820fec221231ca089dcfcccfda6a3f
Ilya Astapov <iastapov114@gmail.com>
2022-04-02 19:49:00 +0300

    Create 1_5Squares.cpp

---
+++ 2SemAlgos/1_5Squares.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+//Вывести квадраты натуральных чисел от 1 до n, используя только O(n) операций сложения и вычитания (умножением пользоваться нельзя).
+//n ≤ 1000.
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    int delta = 3;
+    int number = 1;
+    for (int i = 0; i < n; i++) {
+        cout << number << " ";;
+        number += delta;
+        delta += 2;
+    }
+}
+
