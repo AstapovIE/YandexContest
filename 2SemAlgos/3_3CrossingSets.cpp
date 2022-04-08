d155c2156cd564c8d61f69cfbde84f2ee2bb1d5f
Ilya Astapov <iastapov114@gmail.com>
2022-04-08 17:37:56 +0300

    final version  3_3

--- 2SemAlgos/3_3CrossingSets.cpp
+++ 2SemAlgos/3_3CrossingSets.cpp
@@ -5,19 +5,36 @@ using namespace std;
 //Даны два массива неповторяющихся целых чисел, упорядоченные по возрастанию. A[0..n-1] и B[0..m-1]. n » m. 
 //Найдите их пересечение. Требуемое время работы: O(m * log k), где k - позиция элементта B[m-1] в массиве A.
 //В процессе поиска очередного элемента B[i] в массиве A пользуйтесь результатом поиска элемента B[i-1]. n, k ≤ 10000.
+/*
+6
+4
+1
+4
+7
+8
+11
+15
+1
+-11
+16
+77
+*/
 
-int find_index(vector<int> mass, int x) {
-    int i = mass.size() / 2;
-    int left = 0;
-    int right = mass.size() - 1;
-    while (mass[i] != x && !(mass[i-1] < x && x < mass[i+1])){
+long int find_index(vector<long int> mass, long int x) {
+    long int i = mass.size() / 2;
+    long int left = 0;
+    long int right = mass.size() - 1;
+    if (mass[0] > x) { return -1;  }
+    if (mass[mass.size() - 1] < x) { return -1; }
+    while (mass[i] != x && !(x > mass[i] && x < mass[i+1])){
         if (mass[i] < x) {
             left = i;
             i = i + (right - left) / 2 + 1;
+
         }
         else {
             right = i;
-            i = (right - left) / 2;
+            i = left + (right - left) / 2;
         }
     }
     if (mass[i] == x) {
@@ -26,40 +43,29 @@ int find_index(vector<int> mass, int x) {
     return -1;
 }
 
-vector<int> kick(vector<int> mass, int index) {
-    int buff = 0;
-    for (int i = index; i < mass.size() - 1; i++) {
-        buff = mass[i];
-        mass[i] = mass[i + 1];
-        mass[i + 1] = buff;
-    }
-    mass.pop_back();
-    return mass;
-
-}
 
 int main()
 {
-    int n;
-    int m;
+    long int n;
+    long int m;
     cin >> n;
     cin >> m;
-    vector<int> a;
-    int temp;
-    for (int i = 0; i < n; i++) {
+    vector<long int> a;
+    long int temp;
+    for (long int i = 0; i < n; i++) {
         cin >> temp;
         a.push_back(temp);
     }
-    vector<int> b;
-    for (int i = 0; i < m; i++) {
+    vector<long int> b;
+    for (long int i = 0; i < m; i++) {
         cin >> temp;
         b.push_back(temp);
     }
-    int res = 0;
-    for (int i = 0; i < m; i++) {
+    long int res = 0;
+    for (long int i = 0; i < m; i++) {
         res = find_index(a, b[i]);
         if (res>=0) {
-            cout << a[res] << " ";
+            cout << b[i] << " ";
         }
         
     }
