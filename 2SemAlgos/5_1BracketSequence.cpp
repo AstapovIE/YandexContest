025c6b04547649699b5b928c771ac5bf3c8c59e4
Ilya Astapov <iastapov114@gmail.com>
2022-04-21 21:14:51 +0300

    Create 5_1BracketSequence.cpp

---
+++ 2SemAlgos/5_1BracketSequence.cpp
@@ -0,0 +1,48 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+//Дан фрагмент последовательности скобок, состоящей из символов (){}[].
+//Требуется определить, возможно ли продолжить фрагмент в обе стороны, получив корректную последовательность.
+//Длина исходной последовательности ≤ 800000.
+
+bool is_possible(string str)
+{
+    string open("({[");
+    string close(")}]");
+    int n = str.size() - 1;
+    for (int i = 0; i < n; i++) {
+        if ((str[i] == '(' && str[i + 1] == '}') |
+            (str[i] == '(' && str[i + 1] == ']') |
+            (str[i] == '[' && str[i + 1] == ')') |
+            (str[i] == '[' && str[i + 1] == '}') |
+            (str[i] == '{' && str[i + 1] == ')') |
+            (str[i] == '{' && str[i + 1] == ']'))
+        {
+            return false;
+        }
+
+    }
+    
+    return true;
+}
+
+int main()
+{
+    string seque;
+    string close(")}]");
+    getline(cin, seque);
+    seque.push_back('}');
+    seque.insert(0, ")");
+    cout << seque[0] << endl;
+    cout << seque.size() << endl;
+    int x = seque.find("]");
+    cout << x << endl;
+    x = close.find("}");
+    cout << x << endl;
+    if (is_possible(seque)) {
+        cout << "Dsd";
+    }
+}
+
