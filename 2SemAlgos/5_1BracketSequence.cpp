7b68fa57ed411ef7dc07a77a1847afdda4979509
Ilya Astapov <iastapov114@gmail.com>
2022-04-23 00:11:16 +0300

    Update 5_1BracketSequence.cpp

--- 2SemAlgos/5_1BracketSequence.cpp
+++ 2SemAlgos/5_1BracketSequence.cpp
@@ -1,16 +1,66 @@
 ﻿#include <string>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 //Дан фрагмент последовательности скобок, состоящей из символов (){}[].
 //Требуется определить, возможно ли продолжить фрагмент в обе стороны, получив корректную последовательность.
 //Длина исходной последовательности ≤ 800000.
 
-bool is_possible(string str)
+
+
+/*bool is_possible(string& str)
 {
-    string open("({[");
-    string close(")}]");
+    int open = 0;
+    int close = 0;
+    int n = str.size();
+    for (int i = 0; i < n; i++) {
+        if (str[i] == '(') {
+            int j = i + 1;
+            while (j < n && str[j] != ')')
+            {
+                if (str[j] == '(' | str[j] == '[' | str[j] == '{') {
+                    open++;
+                }
+                else { close++; }
+                if (close > open) { return false; }
+                j++;
+            }
+        }
+        if (str[i] == '{') {
+            int j = i + 1;
+            while (j < n && str[j] != '}')
+            {
+                if (str[j] == '(' | str[j] == '[' | str[j] == '{') {
+                    open++;
+                }
+                else { close++; }
+                if (close > open) { return false; }
+                j++;
+            }
+        }
+        if (str[i] == '[') {
+            int j = i + 1;
+            while (j < n && str[j] != ']')
+            {
+                if (str[j] == '(' | str[j] == '[' | str[j] == '{') {
+                    open++;
+                }
+                else { close++; }
+                if (close > open) { return false; }
+                j++;
+            }
+
+        }
+        //if (close > open) { return false; }
+        open = 0;
+        close = 0;
+    }
+    
+    return true;
+}*/
+
+bool is_possible(string& str)
+{
     int n = str.size() - 1;
     for (int i = 0; i < n; i++) {
         if ((str[i] == '(' && str[i + 1] == '}') |
@@ -24,25 +74,252 @@ bool is_possible(string str)
         }
 
     }
-    
+
     return true;
+}
+
+pair<int, int> find_near(string& str, char open, char close, int index) {
+    pair<int, int> res;
+    res.second = str.size() - 1;
+    bool flag = false;
+    res.first = index;
+    int i = index + 1;
+    while (i <= res.second)
+    {
+        if (str[i] == open) {
+            res.first = i;
+        }
+        if (str[i] == close && !flag) {
+            res.second = i;
+            
+            flag = true;
+        }
+        i++;
+    }
+    if (str[res.second] != close) {
+        res.second = -1;
+    }
+    return res;
+}
+
+void clear(string& str)
+{   
+    bool flag = false;
+    int x = 0;
+    for (int i = 0; i < str.size(); i++) {
+        if (flag) {
+            i = i - 1;
+            flag = false;
+        }
+        if (str[i] == '{') {
+            pair<int, int> to_del = find_near(str, '{', '}', i);
+            if (to_del.second > 0) {
+                str.erase(to_del.first, 1);
+                str.erase(to_del.second - 1, 1);
+                flag = true;
+            }
+        }
+        else {
+            if (str[i] == '(') {
+                pair<int, int> to_del = find_near(str, '(', ')', i);
+                if (to_del.second > 0) {
+                    str.erase(to_del.first, 1);
+                    str.erase(to_del.second - 1, 1);
+                    flag = true;
+                }
+            }
+            else
+            {
+                if (str[i] == '[') {
+                    pair<int, int> to_del = find_near(str, '[', ']', i);
+                    if (to_del.second > 0) {
+                        str.erase(to_del.first, 1);
+                        str.erase(to_del.second - 1, 1);
+                        flag = true;
+                    }
+                }
+            }
+        }
+
+    }
+    //return str;
+ }
+
+void fill(string& first, string& cleaned)
+{
+    int n = cleaned.size();
+    for (int i = 0; i < n; i++) {
+        if (cleaned[i] == ')') {
+            first.insert(0, "(");
+        }
+        if (cleaned[i] == '}') {
+            first.insert(0, "{");
+        }
+        if (cleaned[i] == ']') {
+            first.insert(0, "[");
+        }
+    }
+    for (int i = n-1; i >= 0; i--) {
+        if (cleaned[i] == '(') {
+            first.push_back(')');
+        }
+        if (cleaned[i] == '{') {
+            first.push_back('}');
+        }
+        if (cleaned[i] == '[') {
+            first.push_back(']');
+        }
+    }
+    //return first;
+        
 }
 
 int main()
 {
     string seque;
-    string close(")}]");
+    
     getline(cin, seque);
-    seque.push_back('}');
-    seque.insert(0, ")");
-    cout << seque[0] << endl;
-    cout << seque.size() << endl;
-    int x = seque.find("]");
-    cout << x << endl;
-    x = close.find("}");
-    cout << x << endl;
+    string result = seque;
+    clear(seque);
     if (is_possible(seque)) {
-        cout << "Dsd";
+        //string cl_seque = clear(seque);
+        fill(result, seque);
+        cout << result;
     }
+    else {
+        cout << "IMPOSSIBLE" << endl;
+    }
+    
 }
 
+/*#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool is_possible(string& str)
+{
+    int n = str.size() - 1;
+    for (int i = 0; i < n; i++) {
+        if ((str[i] == '(' && str[i + 1] == '}') |
+            (str[i] == '(' && str[i + 1] == ']') |
+            (str[i] == '[' && str[i + 1] == ')') |
+            (str[i] == '[' && str[i + 1] == '}') |
+            (str[i] == '{' && str[i + 1] == ')') |
+            (str[i] == '{' && str[i + 1] == ']'))
+        {
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+int find_near(string& str, char open, int index) {
+    int max_i = 0;
+    for(int i = 0; i < index; i++){
+        if (str[i] == open) {
+            max_i = i;
+        }
+    }
+    return max_i;
+}
+
+void clear(string& str)
+{   
+    bool flag = false;
+    int x = 0;
+    for (int i = 0; i < str.size(); i++) {
+        if (flag) {
+            i = i - 1;
+            flag = false;
+        }
+        if (str[i] == '{') {
+            x = str.find('}', i);
+            if (x > 0) {
+                int to_del = find_near(str, '{', x);
+                str.erase(to_del, 1);
+                str.erase(x - 1, 1);
+                flag = true;
+                x = 0;
+            }
+        }
+        else {
+            if (str[i] == '(') {
+                x = str.find(')', i);
+                if (x > 0) {
+                    int to_del = find_near(str, '(', x);
+                    str.erase(to_del, 1);
+                    str.erase(x - 1, 1);
+                    flag = true;
+                    x = 0;
+                }
+            }
+            else
+            {
+                if (str[i] == '[') {
+                    x = str.find(']', i);
+                    if (x > 0) {
+                        int to_del = find_near(str, '[', x);
+                        str.erase(to_del, 1);
+                        str.erase(x - 1, 1);
+                        flag = true;
+                        x = 0;
+                    }
+                }
+            }
+        }
+
+    }
+    //return str;
+ }
+
+void fill(string& first, string& cleaned)
+{
+    int n = cleaned.size();
+    for (int i = 0; i < n; i++) {
+        if (cleaned[i] == ')') {
+            first.insert(0, "(");
+        }
+        if (cleaned[i] == '}') {
+            first.insert(0, "{");
+        }
+        if (cleaned[i] == ']') {
+            first.insert(0, "[");
+        }
+    }
+    for (int i = n-1; i >= 0; i--) {
+        if (cleaned[i] == '(') {
+            first.push_back(')');
+        }
+        if (cleaned[i] == '{') {
+            first.push_back('}');
+        }
+        if (cleaned[i] == '[') {
+            first.push_back(']');
+        }
+    }
+    //return first;
+        
+}
+
+int main()
+{
+    string seque;
+    
+    getline(cin, seque);
+    string result = seque;
+    clear(seque);
+    if (is_possible(seque)) {
+        //string cl_seque = clear(seque);
+        fill(result, seque);
+        cout << result;
+    }
+    else {
+        cout << "IMPOSSIBLE" << endl;
+    }
+    
+}
+
+*/
