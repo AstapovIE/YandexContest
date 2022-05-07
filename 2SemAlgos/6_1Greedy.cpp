c5c2ee95bac1939064e5c0b59c66102d059de0f0
Ilya Astapov <iastapov114@gmail.com>
2022-05-07 15:32:36 +0300

    Create 6_1Greedy.cpp

---
+++ 2SemAlgos/6_1Greedy.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+
+int main()
+{
+    map<int, int> m;
+    m.emplace(4, 1);
+    m.emplace(3, 1);
+    m.emplace(5, 1);
+    int max = 6;
+    int k = 0;
+
+    int n = 0;
+    for (auto it = m.rbegin(); it != m.rend(); ++it) {
+        for (auto iter = m.begin(); iter != m.end(); ++iter) {
+            cout << iter->first << " " << iter->second << endl;
+        }
+        n = (max - k) / (it->first);
+        cout << max - k << endl;
+        cout << it->first << endl;
+        cout << n << endl;
+
+        for (int i = 0; i < n; i++) {
+            
+        }
+
+        if (it->first + k <= max) {
+            k += it->first;
+            it->second -= 1;
+            if (it->second == 0) {
+                int temp = it->first;
+                m.erase(temp);
+            }
+        }
+        if (k == max) { break; }
+    }
+    cout << "haha" << endl;
+
+    for (auto iter = m.begin(); iter != m.end(); ++iter) {
+        cout << iter->first << " "<< iter->second << endl;
+    }
+    
+}
