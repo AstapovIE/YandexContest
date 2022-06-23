b90cff29a5e53cd3a8719dbf17e02bb6219ec864
Ilya Astapov <iastapov114@gmail.com>
2022-06-24 01:19:56 +0300

    Create 3_2WidePyramid.cpp

---
+++ 2SemAlgos4/3_2WidePyramid.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+/*Дано N кубиков. Требуется определить каким количеством способов можно выстроить из этих кубиков пирамиду. 
+Каждый вышележащий слой пирамиды должен быть строго меньше нижележащего.*/
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<vector<long long> > matrix(n + 1, vector<long long>(n + 1));
+
+	for (int i = 0; i <= n; ++i)
+		matrix[0][i] = 1;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= i; ++j)
+			matrix[i][j] = matrix[i][j - 1] + matrix[i - j][j - 1];
+		for (int j = i + 1; j <= n; ++j)
+			matrix[i][j] = matrix[i][i];
+	}
+	cout << matrix[n][n];
+}
