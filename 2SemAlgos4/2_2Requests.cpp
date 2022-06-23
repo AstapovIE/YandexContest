dd0c9ebdd812cc3161a186e67475b18b4ec72490
Ilya Astapov <iastapov114@gmail.com>
2022-06-24 00:06:37 +0300

    +2_1, 2_2

---
+++ 2SemAlgos4/2_2Requests.cpp
@@ -0,0 +1,50 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Req {
+	int x;
+	int y;
+
+	Req(int a, int b) {
+		x = a;
+		y = b;
+	}
+};
+
+
+int main()
+{
+	vector<Req> arr;
+
+	int a;
+	int b;
+
+
+
+	while (cin >> a >> b) {
+		arr.push_back(Req(a, b));
+	}
+
+
+	sort(arr.begin(), arr.end(), [](Req a, Req b) {
+		if (a.y < b.y) return true;
+		return false;
+		}
+	);
+
+	int j = 0;
+	int k = 0;
+	for (int i = 1; i < arr.size(); ++i) {
+		if (arr[i].x >= arr[j].y) {
+			++k;
+			j = i;
+		}
+	}
+	cout << k + 1;
+
+
+}
+
