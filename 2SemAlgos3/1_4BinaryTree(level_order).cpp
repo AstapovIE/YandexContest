499ed27d7d178e7335eaa2244f6491ec441901e4
Ilya Astapov <iastapov114@gmail.com>
2022-06-23 21:50:21 +0300

    Create 1_4BinaryTree(level_order).cpp

---
+++ 2SemAlgos3/1_4BinaryTree(level_order).cpp
@@ -0,0 +1,47 @@
+﻿#include <queue>
+#include <iostream>
+
+void BinaryTree::print_elements_level_order()
+{
+    if (head == nullptr) { return; }
+
+    queue<Node*> q;
+    q.push(head);
+
+    while (!q.empty()) {
+        Node* current = q.front();
+        q.pop();
+        cout << current->key << " ";
+
+        if (current->left != nullptr) {
+            q.push(current->left);
+        }
+        if (current->right != nullptr) {
+            q.push(current->right);
+        }
+    }
+}
+
+void BinaryTree::print()
+{
+    print_elements_in_order();
+    cout << endl;
+}
+
+
+int main()
+{
+    BinaryTree a;
+
+    int n;
+    cin >> n;
+
+    int temp;
+    for (int i = 0; i < n; ++i) {
+        cin >> temp;
+        a.insert(temp);
+    }
+
+
+    a.print();
+}
