16a434d31154ce089ed58a860a30fa5a781fdeb0
Ilya Astapov <iastapov114@gmail.com>
2022-06-24 01:19:28 +0300

    Create 3_1HighPyramid.cpp

---
+++ 2SemAlgos4/3_1HighPyramid.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+/*Дано N кубиков. Требуется определить каким количеством способов можно выстроить из этих кубиков пирамиду.
+Каждый вышележащий слой пирамиды должен быть не больше нижележащего.*/
+
+int main() {
+	long long n = 0;
+	cin >> n;
+
+	vector<vector<long long>> arr(n + 1, vector<long long>(n + 1, 0));
+	for (long long i = 0; i < (n + 1); i++) {
+		arr[i][0] = 0;
+		arr[i][1] = 1;
+	}
+
+	for (long long i = 2; i < (n + 1); i++) {
+		for (long long j = 0; j < (n + 1); j++) {
+			arr[j][i] = arr[j][i - 1];
+			if (j - i + 1 > 0) {
+				arr[j][i] += arr[j - i][i];
+			}
+		}
+	}
+
+	cout << arr[n][n];
+}
